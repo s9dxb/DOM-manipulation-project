c42919fe3c588edd522ae6a296842653a75332c2
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2022-10-01 20:56:55 +0200

    cart page css, price update on the cart page

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,5 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
 let calculation = () => {
@@ -5,3 +7,86 @@ let calculation = () => {
   cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
 };
 calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (ShoppingCart.innerHTML = basket
+      .map((x) => {
+        console.log(x);
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+        <div class="cart-item">
+        <img width="100" src=${search.img} alt=""/>
+            <div class="details">
+                <div class="title-price-x">
+                    <h4 class="title-price">
+                        <p>${search.name}</p>
+                        <p class="cart-item-price">$ ${search.price}</p>
+                    </h4>
+                    <i class="bi bi-x-lg"></i>
+
+                </div>
+                        <div class="buttons">
+                            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                                <div id="${id}" class="quantity">${item}</div>
+                            <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                        </div>
+                <h3>$ ${item * search.price}</h3>
+
+            </div>
+        </div>
+        `;
+      })
+      .join(""));
+  } else {
+    ShoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="index.html">
+    <button class="HomeBtn">Back to home</button>
+    </a>
+    `;
+  }
+};
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  localStorage.setItem("data", JSON.stringify(basket));
+  generateCartItems();
+  update(selectedItem.id);
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined)
+    return; //if the search is undefind then the code does nothing
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  localStorage.setItem("data", JSON.stringify(basket)); //to display the objects on the LS
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  update(selectedItem.id);
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
